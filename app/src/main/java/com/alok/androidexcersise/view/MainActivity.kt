8d635a93999d97341499d7228ea923e84eff993a
Alok Soni <asoni@unifytech.com>
2020-11-13 08:04:51 +0530

    Added pull down to refresh feature and screen orientation handling.

--- app/src/main/java/com/alok/androidexcersise/view/MainActivity.kt
+++ app/src/main/java/com/alok/androidexcersise/view/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.alok.androidexcersise.R
 import com.alok.androidexcersise.di.Injection
 import com.alok.androidexcersise.remotedatasource.FeedResponse
+import com.alok.androidexcersise.utils.Constants
 import com.alok.androidexcersise.view.adapter.FeedListAdapter
 import com.alok.androidexcersise.viewmodel.FeedViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.error_layout.*
+
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -22,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var feedViewModel : FeedViewModel
     private lateinit var feedListAdapter: FeedListAdapter
+    private var feedResponse: FeedResponse? = null
+    private var isPulltoRefresh: Boolean = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +37,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        feedViewModel.loadFeedList();
+        if(feedResponse == null) {
+            feedViewModel.loadFeedList();
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putSerializable(Constants.DATA, feedViewModel.feeds.value)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        if (savedInstanceState != null) {
+            feedResponse = savedInstanceState.getSerializable(Constants.DATA) as FeedResponse?
+            feedResponse?.rows?.let { it -> feedListAdapter.update(it) }
+        }
+        super.onRestoreInstanceState(savedInstanceState)
     }
 
     private fun setToolbar(titleValue : String?){
@@ -53,6 +72,15 @@ class MainActivity : AppCompatActivity() {
         feedListAdapter = FeedListAdapter(feedViewModel.feeds.value?.rows?: emptyList())
         feedListRecyclerView.layoutManager = LinearLayoutManager(this)
         feedListRecyclerView.adapter = feedListAdapter
+
+        swipeContainer.setOnRefreshListener {
+            isPulltoRefresh = true
+            feedViewModel.loadFeedList()
+        }
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light);
     }
 
     private val onSuccessObserver = Observer<FeedResponse>{
@@ -60,7 +88,10 @@ class MainActivity : AppCompatActivity() {
         emptyLayout.visibility = View.GONE
         errorLayout.visibility = View.GONE
         noInternetLayout.visibility = View.GONE
+        swipeContainer.isRefreshing = false
+        progressBar.visibility = View.GONE
         setToolbar(it.title)
+        feedResponse = it
         it.rows?.let { it1 -> feedListAdapter.update(it1) }
     }
 
@@ -70,11 +101,18 @@ class MainActivity : AppCompatActivity() {
         emptyLayout.visibility = View.GONE
         errorLayout.visibility = View.GONE
         noInternetLayout.visibility = View.VISIBLE
+        swipeContainer.isRefreshing = false
     }
 
     private val onLoadingObserver = Observer<Boolean> {
         Log.d(TAG, " Result is getting loaded  $it")
-        progressBar.visibility = if(it) View.VISIBLE else View.GONE
+        val visibility = if(it) View.VISIBLE else View.GONE
+        if(visibility == View.VISIBLE){
+            progressBar.visibility = if(!isPulltoRefresh)  View.VISIBLE else View.GONE
+        }else{
+            progressBar.visibility = View.GONE
+            swipeContainer.isRefreshing = false
+        }
     }
 
     private val onEmptyObserver = Observer<Any> {
@@ -82,6 +120,8 @@ class MainActivity : AppCompatActivity() {
         emptyLayout.visibility = View.VISIBLE
         errorLayout.visibility = View.GONE
         noInternetLayout.visibility = View.GONE
+        progressBar.visibility = View.GONE
+        swipeContainer.isRefreshing = false
     }
 
     private val onErrorObserver = Observer<Any> {
@@ -89,6 +129,8 @@ class MainActivity : AppCompatActivity() {
         errorLayout.visibility = View.VISIBLE
         emptyLayout.visibility = View.GONE
         noInternetLayout.visibility = View.GONE
+        progressBar.visibility = View.GONE
+        swipeContainer.isRefreshing = false
         textViewError.text = "$it"
     }
 
